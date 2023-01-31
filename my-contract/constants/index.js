4d7e88cb06fdbb8e1fbbaee90abfc3ca331e1df4
unknown <ibr.s.ahmat@gmail.com>
2023-02-01 02:24:31 +0300

    Setup deployment configuration

---
+++ my-contract/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x1e471D5aB67B800734353aeF1bdb76074D9a4a23";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
