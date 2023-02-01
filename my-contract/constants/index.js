8971805838b478e91637a474bd72f6df6d15db56
unknown <ibr.s.ahmat@gmail.com>
2023-02-02 00:33:24 +0300

    Add METADATA_URL to the contract

--- my-contract/constants/index.js
+++ my-contract/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x1e471D5aB67B800734353aeF1bdb76074D9a4a23";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-ibr5500.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
